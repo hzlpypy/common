a9be43ae457f38ae04d4d7e23f31f0ae53e09571
hzl <1761784585@qq.com>
2021-05-28 16:38:34 +0800

    feat:add get ip

--- example/main.go
+++ example/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	logPathMap := map[string]string{"access": "./log_access.txt", "error": "./log_error.txt"}
+	logPathMap := map[string]string{"access": "./log/log_access.txt", "error": "./log/log_error.txt"}
 	logCfg := &clog.Cfg{}
 	for name, path := range logPathMap {
 		file, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
@@ -25,7 +25,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	l.Access().WithField("test", "info").Info("info")
+	l.Access().Info("info")
 	l.Error().WithField("test", "err123").Error("errwocuola")
 
 }
